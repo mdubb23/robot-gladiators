2e60df500e29f950ba371c9561c430ac3640b935
Matthew <willivms02@hotmail.com>
2022-03-07 12:13:19 -0500

    fight has begun

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("this is an alert! Javascript is running!")
+var playerName = window.prompt("What is your robots name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yorself a message")
+
+//this will do math and log 20
+console.log(10 + 10);
+
+//what is this?
+console.log("our robots name is " + playerName);
+
+function fight() {
+    window.alert("The Fight Has Begun!");
+}
+
+// fight();
